44c9097f0ec39540b5165e166a8fa6ee6e5b1472
sinan akyol <sinanakyol01@gmail.com>
2022-10-20 01:57:53 +0300

    files added

---
+++ js/01.js
@@ -0,0 +1,51 @@
+let listDOM = document.querySelector("#list");
+let ullength = document.getElementsByTagName("li");
+let input = document.querySelector("#input");
+let btnDOM = document.querySelector("#btn");
+
+//MEVCUT LİSTEYİ SİLMEK İÇİN ÇARPI BUTONUNUN OLUŞTURULMASI
+for (let i = 0; i < ullength.length; i++) {
+  let closeBtn = document.createElement("span");
+  closeBtn.textContent = "\uF62A";
+  closeBtn.classList.add("close");
+  closeBtn.onclick = removeButton;
+  ullength[i].append(closeBtn);
+  ullength[i].onclick = check;
+}
+
+btnDOM.addEventListener("click", addElement);
+
+function check() {
+  this.classList.toggle("checked");
+}
+
+function removeButton() {
+  this.parentElement.remove();
+}
+
+function addElement() {
+  if (input.value) {
+    $(".success").toast("show");
+    addItem(input.value);
+    input.value = "";
+  } else {
+    //TOAST'ları çalıştırmak için JQUERY kütüphanesi ekledik.
+    $(".error").toast("show");
+  }
+}
+
+const addItem = (value) => {
+  let liDOM = document.createElement("li");
+  liDOM.innerHTML = value;
+  listDOM.appendChild(liDOM);
+
+  //SONRADAN EKLENEN LİSTEYİ SİLMEK İÇİN
+  liDOM.onclick = check;
+  let closeBtn = document.createElement("span");
+  closeBtn.textContent = "\uF62A";
+  closeBtn.classList.add("close");
+  closeBtn.onclick = removeButton;
+
+  liDOM.append(closeBtn);
+  listDOM.append(liDOM);
+};
